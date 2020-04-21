1c4bdbe0d4edea25ef96fdf4af13e641bd9a8541
Hillal Kumar Roy <hillolroy85@gmail.com>
2020-04-21 14:49:45 +0530

    init react

---
+++ public/src/nav/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class Nav extends React.Component{
+
+    render(){
+
+        return (
+
+            <H1>
+
+                its Working..
+            </H1>
+
+        )
+    }
+
+
+
+
+}
